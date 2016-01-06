1d5af5bd29c5f5030754a9a59e9ae51e07467337
ex1 <ericxiao2@gmail.com>
2016-01-06 11:21:27 -0500

    changed archon to make turrets

--- src/spamturret/ArchonPlayer.java
+++ src/spamturret/ArchonPlayer.java
@@ -6,79 +6,114 @@ import battlecode.common.Clock;
 import battlecode.common.Direction;
 import battlecode.common.GameConstants;
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
 import battlecode.common.Signal;
+import battlecode.common.Team;
+import spamturret.Movement;
 
 public class ArchonPlayer {
-	
+
 	public static void run(RobotController rc) {
 		Random rand = new Random(rc.getID());
+		Team myTeam = rc.getTeam();
+		Team enemyTeam = myTeam.opponent();
+		int numFriendly = 0;
+		RobotInfo[] adjNeutralRobots = rc.senseNearbyRobots(2, Team.NEUTRAL);
+
 		try {
-            // Any code here gets executed exactly once at the beginning of the game.
-        } catch (Exception e) {
-            // Throwing an uncaught exception makes the robot die, so we need to catch exceptions.
-            // Caught exceptions will result in a bytecode penalty.
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+			// Any code here gets executed exactly once at the beginning of the game.
+		} catch (Exception e) {
+			// Throwing an uncaught exception makes the robot die, so we need to catch exceptions.
+			// Caught exceptions will result in a bytecode penalty.
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 
-        while (true) {
-            // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
-            // at the end of it, the loop will iterate once per game round.
-            try {
-                int fate = rand.nextInt(1000);
-                // Check if this ARCHON's core is ready
-                if (fate % 10 == 2) {
-                    // Send a message signal containing the data (6370, 6147)
-                    rc.broadcastMessageSignal(6370, 6147, 80);
-                }
-                Signal[] signals = rc.emptySignalQueue();
-                if (signals.length > 0) {
-                    // Set an indicator string that can be viewed in the client
-                    rc.setIndicatorString(0, "I received a signal this turn!");
-                } else {
-                    rc.setIndicatorString(0, "I don't any signal buddies");
-                }
-                if (rc.isCoreReady()) {
-                    if (fate < 200) {
-                        // Choose a random direction to try to move in
-                        Direction dirToMove = RobotPlayer.directions[fate % 8];
-                        // Check the rubble in that direction
-                        if (rc.senseRubble(rc.getLocation().add(dirToMove)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
-                            // Too much rubble, so I should clear it
-                            rc.clearRubble(dirToMove);
-                            // Check if I can move in this direction
-                        } else if (rc.canMove(dirToMove)) {
-                            // Move
-                            rc.move(dirToMove);
-                        }
-                    } else {
-                        // Choose a random unit to build
-                        RobotType typeToBuild = RobotType.TURRET;
-                        // Check for sufficient parts
-                        if (rc.hasBuildRequirements(typeToBuild)) {
-                            // Choose a random direction to try to build in
-                            Direction dirToBuild = RobotPlayer.directions[rand.nextInt(8)];
-                            for (int i = 0; i < 8; i++) {
-                                // If possible, build in this direction
-                                if (rc.canBuild(dirToBuild, typeToBuild)) {
-                                    rc.build(dirToBuild, typeToBuild);
-                                    break;
-                                } else {
-                                    // Rotate the direction to try
-                                    dirToBuild = dirToBuild.rotateLeft();
-                                }
-                            }
-                        }
-                    }
-                }
+		while (true) {
+			// This is a loop to prevent the run() method from returning. Because of the Clock.yield()
+			// at the end of it, the loop will iterate once per game round.
 
-                Clock.yield();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
-        }
+			try {
+				boolean escape = false;
+				if (rc.isCoreReady()) {
+					escape = Movement.moveAwayFromEnemy(rc);
+				}
+				if (!escape) {
+					if (adjNeutralRobots.length > 0){
+						//if there is a neutral robot adjacent, activate it or wait until there's no core delay
+						if (rc.isCoreReady()) {
+							rc.activate(adjNeutralRobots[0].location);
+						}            			
+					}
+					else {
+						boolean toheal = false;
+						//repair a nearby friendly robot
+						if (rc.isWeaponReady()) {
+							RobotInfo[] friendlyWithinRange = rc.senseNearbyRobots(24, myTeam);
+							if (numFriendly != friendlyWithinRange.length) {
+								numFriendly = friendlyWithinRange.length;
+							}
+
+							if (friendlyWithinRange.length > 0) {
+								RobotInfo toRepair = friendlyWithinRange[0];
+								for (RobotInfo r : friendlyWithinRange ) {
+									if ((r.health < toRepair.health) && (r.type != RobotType.ARCHON)) {
+										toRepair = r;
+									}
+								}
+								if ((toRepair.maxHealth-toRepair.health > 1) && (toRepair.type != RobotType.ARCHON)) {
+									toheal = true;
+									rc.repair(toRepair.location);
+								}
+							}
+						}
+						if (toheal == false) {
+							// Check if this ARCHON's core is ready
+							if (rc.isCoreReady()) {
+								boolean built = false;
+								// sometimes build soldier
+								RobotType typeToBuild = RobotType.TURRET;
+								// Check for sufficient parts
+								if (rc.hasBuildRequirements(typeToBuild)) {
+									// Choose a random direction to try to build in; NESW
+									Direction dirToBuild = RobotPlayer.directions[rand.nextInt(4)*2];
+									for (int i = 0; i < 4; i++) {
+										// If possible, build in this direction
+										if (rc.canBuild(dirToBuild, typeToBuild)) {
+											rc.build(dirToBuild, typeToBuild);
+											built = true;
+											break;
+										} else {
+											// Rotate the direction to try
+											dirToBuild = dirToBuild.rotateLeft();
+											dirToBuild = dirToBuild.rotateLeft();
+										}
+									}
+								}
+								if (!built) {
+									Direction dirToMove = RobotPlayer.directions[(rand.nextInt(4)*2) + 1];
+									for (int i = 0; i < 4; i++) {
+										if (rc.canMove(dirToMove)) {
+											rc.move(dirToMove);
+											break;
+										}
+										else {
+											dirToMove = dirToMove.rotateLeft();
+											dirToMove = dirToMove.rotateLeft();
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				Clock.yield();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
