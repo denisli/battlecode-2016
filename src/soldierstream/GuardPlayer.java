e6c1bfc2dfae627e95b1dc04a6bcd450a3832a40
Martin Zhu <martin_zhu@brown.edu>
2016-01-07 12:50:35 -0500

    guard bug around archon

--- src/soldierstream/GuardPlayer.java
+++ src/soldierstream/GuardPlayer.java
@@ -1,5 +1,6 @@
 package soldierstream;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import battlecode.common.Clock;
@@ -7,7 +8,9 @@ import battlecode.common.Direction;
 import battlecode.common.GameConstants;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
 import battlecode.common.Team;
+import soldierstream.RobotPlayer;
 
 public class GuardPlayer {
 	public static void run(RobotController rc) {
@@ -30,28 +33,72 @@ public class GuardPlayer {
             // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
             // at the end of it, the loop will iterate once per game round.
             try {
-                int fate = rand.nextInt(1000);
-
-                boolean shouldAttack = false;
-
                 // If this robot type can attack, check for enemies within range and attack one
-                if (myAttackRange > 0) {
-                    RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(myAttackRange, enemyTeam);
-                    RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(myAttackRange, Team.ZOMBIE);
-                    //sort arrays to attack enemy with least health?
-                    if (enemiesWithinRange.length > 0) {
-                        shouldAttack = true;
-                        // Check if weapon is ready
-                        if (rc.isWeaponReady()) {
-                            rc.attackLocation(enemiesWithinRange[rand.nextInt(enemiesWithinRange.length)].location);
+                RobotInfo[] enemiesWithinRange = rc.senseHostileRobots(rc.getLocation(), rc.getType().sensorRadiusSquared);
+                
+                if (enemiesWithinRange.length > 0) {
+                	RobotInfo nearestEnemy = enemiesWithinRange[0];
+                	for (RobotInfo r : enemiesWithinRange) {
+                		if (r.location.distanceSquaredTo(rc.getLocation()) < nearestEnemy.location.distanceSquaredTo(rc.getLocation())) {
+                			nearestEnemy = r;
+                		}
+                	}
+                    // Check if weapon is ready
+                    if (rc.isWeaponReady() && rc.canAttackLocation(nearestEnemy.location)) {
+                        rc.attackLocation(nearestEnemy.location);
+                    } else { // otherwise try to move towards the enemy
+                    	RobotInfo[] friendliesWithinRange = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, myTeam);
+                        ArrayList<RobotInfo> archonsWithinRange = new ArrayList<>();
+                        for (RobotInfo r : friendliesWithinRange) {
+                        	if (r.type == RobotType.ARCHON) {
+                        		archonsWithinRange.add(r);
+                        	}
                         }
-                    } else if (zombiesWithinRange.length > 0) {
-                        shouldAttack = true;
-                        // Check if weapon is ready
-                        if (rc.isWeaponReady()) {
-                            rc.attackLocation(zombiesWithinRange[rand.nextInt(zombiesWithinRange.length)].location);
+                        if (archonsWithinRange.size() > 0) {
+                        	RobotInfo nearestArchon = archonsWithinRange.get(0);
+                        	for (RobotInfo r : archonsWithinRange) {
+                        		if (r.location.distanceSquaredTo(rc.getLocation()) < nearestArchon.location.distanceSquaredTo(rc.getLocation())) {
+                        			nearestArchon = r;
+                        		}
+                        	}
+                        	if (rc.isCoreReady()) {
+                        		// try to move towards enemy
+                        		Direction dirToMove = rc.getLocation().directionTo(nearestEnemy.location);
+                        		if (rc.canMove(dirToMove) && rc.getLocation().add(dirToMove).distanceSquaredTo(nearestArchon.location) <= 5) {
+	   	                            // Move
+	   	                            rc.move(dirToMove);
+	   	                        }
+                           }
                         }
                     }
+                } else { // if no enemies, it should try to circle nearest archon
+                	RobotInfo[] friendliesWithinRange = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, myTeam);
+                    ArrayList<RobotInfo> archonsWithinRange = new ArrayList<>();
+                    for (RobotInfo r : friendliesWithinRange) {
+                    	if (r.type == RobotType.ARCHON) {
+                    		archonsWithinRange.add(r);
+                    	}
+                    }
+                    if (archonsWithinRange.size() > 0) {
+                    	RobotInfo nearestArchon = archonsWithinRange.get(0);
+                    	for (RobotInfo r : archonsWithinRange) {
+                    		if (r.location.distanceSquaredTo(rc.getLocation()) < nearestArchon.location.distanceSquaredTo(rc.getLocation())) {
+                    			nearestArchon = r;
+                    		}
+                    	}
+                    	if (rc.isCoreReady()) {
+                       	 // Choose a random direction to try to move in
+	                       	for (int i = 0; i < 8; i++) {
+	   	                        Direction dirToMove = RobotPlayer.directions[rand.nextInt(1000) % 8];
+	   	                        if (rc.canMove(dirToMove) && rc.getLocation().add(dirToMove).distanceSquaredTo(nearestArchon.location) <= 5 && rc.isCoreReady()) {
+	   	                            // Move
+	   	                            rc.move(dirToMove);
+	   	                            Clock.yield();
+	   	                        }
+	                        }
+                       }
+                    }
+                    
                 }
 
                 Clock.yield();
