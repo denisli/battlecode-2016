26531ac3d858cdac171788050fbc2fc4e129e976
Denis Li <denisli@mit.edu>
2016-01-07 13:33:22 -0500

    Soldier vs soldier micro

--- src/soldierstream/SoldierPlayer.java
+++ src/soldierstream/SoldierPlayer.java
@@ -14,6 +14,8 @@ public class SoldierPlayer {
 	static Bugging bugging = null;
 	static MapLocation storedNearestDen = null;
 	static MapLocation storedNearestArchon = null;
+	static boolean committedMicro = false;
+	
 	public static void run(RobotController rc) {
 		int myAttackRange = 0;
 		Random rand = new Random(rc.getID());
@@ -40,6 +42,8 @@ public class SoldierPlayer {
             	MapLocation myLoc = rc.getLocation();
             	int fate = rand.nextInt(1000);
                 boolean shouldAttack = false;
+                boolean useSoldierMicro = false;
+                double totalEnemySoldierHealth = 0;
                 
                 // take a look at all hostile robots within the sight radius
                 RobotInfo[] enemiesWithinRange = rc.senseHostileRobots(myLoc, rc.getType().sensorRadiusSquared);
@@ -52,12 +56,17 @@ public class SoldierPlayer {
                 	Direction turretDir = null;
           
                     for (RobotInfo r: enemiesWithinRange) {
-                    	if (r.type == RobotType.TURRET) {
+                    	if (r.type == RobotType.SOLDIER) {
+                    		// Use soldier micro
+                    		useSoldierMicro = true;
+                    		totalEnemySoldierHealth += r.health;
+                    	}
+                    	else if (r.type == RobotType.TURRET) {
                     		//check if there's a turret in range; if there is, code later prioritizes moving away from turret
                     		//later fix so that if there's 1 turret, move towards it and kill it
                     		turretDir = myLoc.directionTo(r.location);
                     	}
-                    	if (r.type == RobotType.ARCHON) {
+                    	else if (r.type == RobotType.ARCHON) {
                     		//it sees archon
                     		if (bestEnemy.type == RobotType.ARCHON) {
                     			if (r.health < bestEnemy.health) {
@@ -88,28 +97,115 @@ public class SoldierPlayer {
                     		rc.move(turretDir.opposite());
                     	}
                     }
-                    // if we are too close, we want to move further away
-                    if (myLoc.distanceSquaredTo(bestEnemy.location) < 8 && rc.isCoreReady()) {
-                		if (rc.canMove(d.opposite())) {
-                			rc.move(d.opposite());
-                		} else if (rc.canMove(d.opposite().rotateLeft())) {
-                			rc.move(d.opposite().rotateLeft());
-                		} else if (rc.canMove(d.opposite().rotateRight())) {
-                			rc.move(d.opposite().rotateRight());
-                		}
-                	} else if (myLoc.distanceSquaredTo(bestEnemy.location) > 13 && rc.isCoreReady()) { // if we are too far, we want to move closer
-                		if (rc.canMove(d)) {
-                			rc.move(d);
-                		} else if (rc.canMove(d.rotateLeft())) {
-                			rc.move(d.rotateLeft());
-                		} else if (rc.canMove(d.rotateRight())) {
-                			rc.move(d.rotateRight());
-                		}
-                	} else { // otherwise we want to try to attack
-                		if (rc.isWeaponReady() && rc.canAttackLocation(bestEnemy.location)) {
-                			rc.attackLocation(bestEnemy.location);
-                		}
-                	}
+                    if (useSoldierMicro) {
+                    	// Attack whenever you can
+                    	if (rc.isWeaponReady()) {
+                    		if (rc.canAttackLocation(bestEnemy.location)) {
+                    			rc.attackLocation(bestEnemy.location);
+                    		}
+                    	}
+                    	if (rc.isCoreReady()) {
+                    		// If can back away from soldier hit, then do it!
+                    		Direction bestBackAwayDir = Direction.NONE;
+                    		int bestBackAwayDist = 1000;
+                    		// Pick the direction that gets away from soldier attack, and minimizes that dist.
+                    		if (rc.canMove(d.opposite())) {
+                    			int backAwayDist = myLoc.add(d.opposite()).distanceSquaredTo(bestEnemy.location);
+	                			if (backAwayDist > RobotType.SOLDIER.attackRadiusSquared) {
+	                				if (backAwayDist < bestBackAwayDist) {
+	                					bestBackAwayDist = backAwayDist;
+	                					bestBackAwayDir = d.opposite();
+	                				}
+	                			}
+	                		} else if (rc.canMove(d.opposite().rotateLeft())) {
+	                			int backAwayDist = myLoc.add(d.opposite().rotateLeft()).distanceSquaredTo(bestEnemy.location);
+	                			if (backAwayDist > RobotType.SOLDIER.attackRadiusSquared) {
+	                				if (backAwayDist < bestBackAwayDist) {
+	                					bestBackAwayDist = backAwayDist;
+	                					bestBackAwayDir = d.opposite().rotateLeft();
+	                				}
+	                			}
+	                		} else if (rc.canMove(d.opposite().rotateRight())) {
+	                			int backAwayDist = myLoc.add(d.opposite().rotateRight()).distanceSquaredTo(bestEnemy.location);
+	                			if (backAwayDist > RobotType.SOLDIER.attackRadiusSquared) {
+	                				if (backAwayDist < bestBackAwayDist) {
+	                					bestBackAwayDist = backAwayDist;
+	                					bestBackAwayDir = d.opposite().rotateRight();
+	                				}
+	                			}
+	                		}
+                    		if (bestBackAwayDir != Direction.NONE) {
+                    			rc.move(bestBackAwayDir);
+                    		} else {
+		                    	// If the enemy can be killed in one hit but we're not in range, move forward
+	                    		if (rc.getHealth() > RobotType.SOLDIER.attackPower) {
+			                    	if (myLoc.distanceSquaredTo(bestEnemy.location) > 13 && bestEnemy.health < RobotType.SOLDIER.attackPower) {
+			                    		if (rc.canMove(d)) {
+				                			rc.move(d);
+				                		} else if (rc.canMove(d.rotateLeft())) {
+				                			rc.move(d.rotateLeft());
+				                		} else if (rc.canMove(d.rotateRight())) {
+				                			rc.move(d.rotateRight());
+				                		}
+			                    	// If not in range, see if we should move in by comparing soldier health
+			                    	} else {
+			                    		double totalOurSoldierHealth = 0;
+			                    		RobotInfo[] allies = rc.senseNearbyRobots(bestEnemy.location, 21, rc.getTeam());
+			                    		for (RobotInfo ally : allies) {
+			                    			if (ally.type == RobotType.SOLDIER) {
+			                    				totalOurSoldierHealth += ally.health;
+			                    			}
+			                    		}
+			                    		// If we feel that we are strong enough, rush in.
+			                    		if (3 * totalOurSoldierHealth > 4 * totalEnemySoldierHealth || committedMicro) {
+			                    			if (!rc.canAttackLocation(bestEnemy.location)) {
+				                    			if (rc.canMove(d)) {
+						                			rc.move(d);
+						                		} else if (rc.canMove(d.rotateLeft())) {
+						                			rc.move(d.rotateLeft());
+						                		} else if (rc.canMove(d.rotateRight())) {
+						                			rc.move(d.rotateRight());
+						                		}
+				                    			committedMicro = true;
+			                    			}
+			                    		} else {
+			                    			if (rc.canMove(d.opposite())) {
+			    	                			rc.move(d.opposite());
+			    	                		} else if (rc.canMove(d.opposite().rotateLeft())) {
+			    	                			rc.move(d.opposite().rotateLeft());
+			    	                		} else if (rc.canMove(d.opposite().rotateRight())) {
+			    	                			rc.move(d.opposite().rotateRight());
+			    	                		}
+			                    		}
+		                    		}
+		                    	}
+                    		}
+                    	}
+                    } else {
+                    	committedMicro = false;
+	                    // if we are too close, we want to move further away
+	                    if (myLoc.distanceSquaredTo(bestEnemy.location) < 8 && rc.isCoreReady()) {
+	                		if (rc.canMove(d.opposite())) {
+	                			rc.move(d.opposite());
+	                		} else if (rc.canMove(d.opposite().rotateLeft())) {
+	                			rc.move(d.opposite().rotateLeft());
+	                		} else if (rc.canMove(d.opposite().rotateRight())) {
+	                			rc.move(d.opposite().rotateRight());
+	                		}
+	                	} else if (myLoc.distanceSquaredTo(bestEnemy.location) > 13 && rc.isCoreReady()) { // if we are too far, we want to move closer
+	                		if (rc.canMove(d)) {
+	                			rc.move(d);
+	                		} else if (rc.canMove(d.rotateLeft())) {
+	                			rc.move(d.rotateLeft());
+	                		} else if (rc.canMove(d.rotateRight())) {
+	                			rc.move(d.rotateRight());
+	                		}
+	                	} else { // otherwise we want to try to attack
+	                		if (rc.isWeaponReady() && rc.canAttackLocation(bestEnemy.location)) {
+	                			rc.attackLocation(bestEnemy.location);
+	                		}
+	                	}
+                    }
                 }
                 
 
