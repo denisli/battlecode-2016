39ee5c67828f51664ac7ac3103b1a55f240b6d4f
Denis Li <denisli@mit.edu>
2016-01-07 11:34:43 -0500

    Fixed/improved bugging code

--- src/soldierstream/Bugging.java
+++ src/soldierstream/Bugging.java
@@ -26,26 +26,32 @@ public class Bugging {
 			if (rc.canMove(dir)) {
 				rc.move(dir);
 			} else {
-				// Since we can't move closer to the destination, we should commence hugging.
+				// Since we can't move closer to the destination, we should
+				// commence hugging.
 				// First we find out which direction of hugging is faster.
-				
+
 				// Compute the distance assuming hugging right
+				int numRotates = 0;
 				Direction rightHugDir = dir.rotateLeft();
-				while (!rc.canMove(rightHugDir)) {
+				while (!rc.canMove(rightHugDir) && numRotates < 8) {
 					rightHugDir = rightHugDir.rotateLeft();
+					numRotates++;
 				}
 				MapLocation rightHugLoc = myLocation.add(rightHugDir);
 				int rightHugDist = rightHugLoc.distanceSquaredTo(destination);
-				
+
 				// Compute the distance assuming hugging left
+				numRotates = 0;
 				Direction leftHugDir = dir.rotateRight();
-				while (!rc.canMove(leftHugDir)) {
+				while (!rc.canMove(leftHugDir) && numRotates < 8) {
 					leftHugDir = leftHugDir.rotateRight();
+					numRotates++;
 				}
 				MapLocation leftHugLoc = myLocation.add(leftHugDir);
 				int leftHugDist = leftHugLoc.distanceSquaredTo(destination);
-				
-				// Pick the hugging which gives the least distance towards the destination
+
+				// Pick the hugging which gives the least distance towards the
+				// destination
 				if (rightHugDist < leftHugDist) {
 					hugging = Hugging.RIGHT;
 					dirWhileHugging = rightHugDir;
@@ -53,21 +59,24 @@ public class Bugging {
 					hugging = Hugging.LEFT;
 					dirWhileHugging = leftHugDir;
 				}
-				
+
 				// Complete the move.
-				rc.move(dirWhileHugging);
+				if (rc.canMove(dirWhileHugging)) {
+					rc.move(dirWhileHugging);
+				}
 			}
 		} else {
 			if (hugging == Hugging.LEFT) {
 				// Check to see if the robot can move towards the destination.
-				// If the direction is fan = 1 away from the direction it came from,
+				// If the direction is fan = 1 away from the direction it came
+				// from,
 				// then do NOT break out of hugging. Else get out of hugging.
 				MapLocation myLocation = rc.getLocation();
 				Direction dirToDest = myLocation.directionTo(destination);
 				Direction cameFromDir = dirWhileHugging.opposite();
-				
+
 				// In this case, break out of bugging
-				if (getFanDist(dirToDest, cameFromDir) > 1) {
+				if (getFanDist(dirToDest, cameFromDir) > 1 && rc.canMove(dirToDest)) {
 					hugging = Hugging.NONE;
 					rc.move(dirToDest);
 				// Continue to bug...
@@ -78,21 +87,24 @@ public class Bugging {
 						dirWhileHugging = dirWhileHugging.rotateRight();
 						numRotates++;
 					}
-					rc.move(dirWhileHugging);
+					if (rc.canMove(dirWhileHugging)) {
+						rc.move(dirWhileHugging);
+					}
 				}
-			} else { // hugging = Hugging.RIGHT  MOSTLY COPY PASTA FROM ABOVE
+			} else { // hugging = Hugging.RIGHT MOSTLY COPY PASTA FROM ABOVE
 				// Check to see if the robot can move towards the destination.
-				// If the direction is fan = 1 away from the direction it came from,
+				// If the direction is fan = 1 away from the direction it came
+				// from,
 				// then do NOT break out of hugging. Else get out of hugging.
 				MapLocation myLocation = rc.getLocation();
 				Direction dirToDest = myLocation.directionTo(destination);
 				Direction cameFromDir = dirWhileHugging.opposite();
-				
+
 				// In this case, break out of bugging
-				if (getFanDist(dirToDest, cameFromDir) > 1) {
+				if (getFanDist(dirToDest, cameFromDir) > 1 && rc.canMove(dirToDest)) {
 					hugging = Hugging.NONE;
 					rc.move(dirToDest);
-				// Continue to bug...
+					// Continue to bug...
 				} else {
 					dirWhileHugging = dirWhileHugging.rotateRight();
 					int numRotates = 0;
@@ -100,20 +112,22 @@ public class Bugging {
 						dirWhileHugging = dirWhileHugging.rotateLeft();
 						numRotates++;
 					}
-					rc.move(dirWhileHugging);
+					if (rc.canMove(dirWhileHugging)) {
+						rc.move(dirWhileHugging);
+					}
 				}
 			}
 		}
 	}
-	
+
 	private static enum Hugging {
 		LEFT, RIGHT, NONE; // NONE means not bugging
 	}
-	
+
 	private int getFanDist(Direction dir1, Direction dir2) {
 		return Math.abs(getDirTurnsAwayFrom4(dir1) - getDirTurnsAwayFrom4(dir2));
 	}
-	
+
 	// Number of turns away from the 4 dir.
 	private int getDirTurnsAwayFrom4(Direction dir) {
 		return Math.abs(dir.ordinal() - 4);
