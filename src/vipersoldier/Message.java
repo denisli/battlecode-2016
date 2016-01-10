397b8d0293bea91e574b3fce421c7c2b1aa0fab3
Eric Xiao <eric_xiao@brown.edu>
2016-01-09 19:23:13 -0500

    new signal strategy

--- src/vipersoldier/Message.java
+++ src/vipersoldier/Message.java
@@ -10,21 +10,29 @@ import battlecode.common.Signal;
 
 public class Message {
 	
+	public static final int BASIC = -1;
 	public static final int DEN = 0;
 	public static final int SWARM = 1;
 	public static final int ENEMY = 2;
+	public static final int DELETEDEN = 3;
+	//tells all other scouts to not go to basic signal location anymore
+	public static final int REMOVEBASICSIGNAL = 4;
+	
 	
 	private static final int D = 20000;
 	private static final int AYY = 50000;
 	
+	public final Signal signal;
 	public final MapLocation location;
 	public final int type;
 
-	public Message(MapLocation location, int type) {
+	public Message(Signal signal, MapLocation location, int type) {
+		this.signal = signal;
 		this.location = location;
 		this.type = type;
 	}
 	
+	//used only for scouts
 	public static void sendMessage(RobotController rc, MapLocation location, int type, int range) throws GameActionException {
 		int x = location.x + D + type * AYY;
 		int y = location.y + D + type * AYY;
@@ -38,9 +46,13 @@ public class Message {
 			boolean isOurMessage = signal.getTeam().equals(rc.getTeam());
 			if (isOurMessage) {
 				int[] signalMessage = signal.getMessage();
-				int x = signalMessage[0], y = signalMessage[1];
-				int type = x / AYY;
-				messages.add(new Message(new MapLocation(x - D - type * AYY, y - D - type * AYY), type));
+				if (signalMessage != null) {
+					int x = signalMessage[0], y = signalMessage[1];
+					int type = x / AYY;
+					messages.add(new Message(signal, new MapLocation(x - D - type * AYY, y - D - type * AYY), type));
+				} else {
+					messages.add(new Message(signal, null, BASIC));
+				}
 			}
 			signal = rc.readSignal();
 		}
