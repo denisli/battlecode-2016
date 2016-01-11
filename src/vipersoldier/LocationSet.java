362597cfba845bbf8b724ba79bc466b13757e18e
Eric Xiao <eric_xiao@brown.edu>
2016-01-11 16:23:49 -0500

    doms

--- src/vipersoldier/LocationSet.java
+++ src/vipersoldier/LocationSet.java
@@ -13,7 +13,7 @@ public class LocationSet implements Iterable<MapLocation> {
 	private final int[][] containsLocation = new int[200][200];
 	private final MapLocation[] mapLocations = new MapLocation[500]; // assume 500 locations max. Otherwise we done goofed.
 	private int index = 1;
-	private int size;
+	private int size = 0;
 	
 	private final Iterator<MapLocation> iterator = new Iterator<MapLocation>() {
 
@@ -38,7 +38,7 @@ public class LocationSet implements Iterable<MapLocation> {
 		mapLocations[size] = location;
 		int x = location.x % 100 + 100, y = location.y % 100 + 100;
 		if (containsLocation[x][y] == 0) {
-			containsLocation[x][y] = size++;
+			containsLocation[x][y] = ++size;
 		}
 	}
 	
@@ -47,7 +47,11 @@ public class LocationSet implements Iterable<MapLocation> {
 		int x = location.x % 100 + 100, y = location.y % 100 + 100;
 		int index = containsLocation[x][y];
 		if (index == 0) return false; // no location removed
-		mapLocations[index-1] = mapLocations[--size];
+		MapLocation swapLocation = mapLocations[--size];
+		mapLocations[index-1] = swapLocation;
+		containsLocation[x][y] = 0; // set the index back to 0 for whatever was removed
+		int swapX = swapLocation.x % 100 + 100, swapY = swapLocation.y % 100 + 100;
+		containsLocation[swapX][swapY] = index; // change the swapped location's index
 		return true;
 		
 	}
