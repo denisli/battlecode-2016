6d6a2e55ee5f0822fa551c048e582c459071c39c
Martin Zhu <martin_zhu@brown.edu>
2016-01-15 11:52:15 -0500

    added viper and guard

---
+++ src/sprintplus/GuardPlayer.java
@@ -0,0 +1,20 @@
+package sprintplus;
+
+import battlecode.common.*;
+
+public class GuardPlayer {
+	
+	public static void run(RobotController rc) {
+		while (true) {
+			try {
+				
+				
+				
+				Clock.yield();
+			} catch (Exception e) {
+				
+			}
+		}
+	}
+
+}
