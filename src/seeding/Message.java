28ef6d07abe46aac1abe0dd003c9437725dbf685
Martin Zhu <martin_zhu@brown.edu>
2016-01-16 12:57:11 -0500

    new package for seeding tournament

---
+++ src/seeding/Message.java
@@ -0,0 +1,89 @@
+package seeding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.Signal;
+
+public class Message {
+	
+	public static final int UNPAIRED = 1;
+	public static final int PAIRED = 2;
+	public static final int ZOMBIEDEN = 3;
+	public static final int ZOMBIE = 4;
+	public static final int TURRET = 5;
+	public static final int TURRETKILLED = 6;
+	public static final int ENEMY = 7;
+	public static final int COLLECTIBLES = 8;
+	public static final int ENEMYARCHONLOC = 9; // enemy archon loc
+	public static final int RUSH = 10;
+	public static final int ARCHONLOC = 11; // our archon loc
+	public static final int RUSHNOTURRET = 12;
+	public static final int PAIREDATTACK = 13;
+	//scout sends to turret if it sees enemy turret+scout
+	public static final int ENEMYTURRETSCOUT = 14;
+	public static final int ARCHONINDANGER = 15;
+	
+	private static final int D = 20000;
+	private static final int AYY = 50000;
+	
+	public static final int FULL_MAP_RANGE = 80 * 80 * 2;
+	
+	public final Signal signal;
+	public final MapLocation location;
+	public final int type;
+
+	public Message(Signal signal, MapLocation location, int type) {
+		this.signal = signal;
+		this.location = location;
+		this.type = type;
+	}
+	
+	public static void sendMessageGivenRange(RobotController rc, MapLocation location, int type, int range) throws GameActionException {
+		int x = location.x + D + type * AYY;
+		int y = location.y + D + type * AYY;
+		rc.broadcastMessageSignal(x, y, range);
+	}
+	
+	public static void sendMessageGivenDelay(RobotController rc, MapLocation location, int type, double delay) throws GameActionException {
+		int range = getRangeGivenDelay(rc, delay);
+		sendMessageGivenRange(rc, location, type, range);
+	}
+	
+	public static List<Message> readMessageSignals(RobotController rc) {
+		List<Message> messages = new ArrayList<Message>();
+		Signal signal = rc.readSignal();
+		while (signal != null) {
+			boolean isOurMessage = signal.getTeam().equals(rc.getTeam());
+			if (isOurMessage) {
+				int[] signalMessage = signal.getMessage();
+				int x = signalMessage[0], y = signalMessage[1];
+				int type = x / AYY;
+				messages.add(new Message(signal, new MapLocation(x - D - type * AYY, y - D - type * AYY), type));
+			}
+			signal = rc.readSignal();
+		}
+		return messages;
+	}
+	
+	public boolean equals(Object other) {
+		if (!(other instanceof Message)) {
+			return false;
+		} else {
+			Message o = (Message) other;
+			return o.location.equals(location) && o.type == type;
+		}
+	}
+	
+	public int hashCode() {
+		return location.hashCode() + type;
+	}
+	
+	private static int getRangeGivenDelay(RobotController rc, double delay) {
+		return (int) ((delay - 0.05) / 0.03 + 2) * rc.getType().sensorRadiusSquared;
+	}
+	
+}
---
+++ src/seeding/Movement.java
@@ -0,0 +1,185 @@
+package seeding;
+
+import java.util.Set;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+import battlecode.common.Team;
+
+public class Movement {
+
+	public static Direction getBestMoveableDirection(Direction dir, RobotController rc, int fan) {
+		int ordinal = dir.ordinal();
+		for (int i = 0; i < 2 * fan + 1; i++) {
+			int disp = ((i % 2) * 2 - 1) * (i+1) / 2;
+			Direction testDir = Direction.values()[mod8(ordinal + disp)];
+			if ( rc.canMove(testDir) ) {
+				return testDir;
+			}
+		}
+		return Direction.NONE;
+	}
+	
+	//returns true if the robot moved away
+	public static boolean moveAwayFromEnemy(RobotController rc) throws GameActionException {
+		int mySightRange = rc.getType().sensorRadiusSquared;
+		MapLocation myLoc = rc.getLocation();
+		RobotInfo[] hostiles = rc.senseHostileRobots(myLoc, mySightRange);	
+		MapLocation closestEnemy = null;
+		int closestEnemyDist = 60;
+		for (RobotInfo e : hostiles) {
+			if (e.type == RobotType.ZOMBIEDEN) continue;
+			MapLocation curEnemyLoc = e.location;
+			int curDist = myLoc.distanceSquaredTo(curEnemyLoc);
+			if (curDist < closestEnemyDist) {
+				closestEnemyDist = curDist;
+				closestEnemy = e.location;
+			}
+		}
+		if (closestEnemy == null) {
+			return false;
+		}
+		else {
+			Direction dir = getBestMoveableDirection(closestEnemy.directionTo(myLoc), rc, 4);
+			if (dir != Direction.NONE) {
+				rc.move(dir);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	
+	public static boolean getToAdjParts(RobotController rc) throws GameActionException {
+		if (rc.isCoreReady()) {
+			MapLocation myLoc = rc.getLocation();
+			MapLocation[] squaresAdj = MapLocation.getAllMapLocationsWithinRadiusSq(rc.getLocation(), 2);
+			for (MapLocation sq : squaresAdj) {
+				if ((rc.senseParts(sq) > 0) && (rc.canMove(myLoc.directionTo(sq)))) {
+					rc.move(myLoc.directionTo(sq));
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	//moves to parts/neutral robots in sight radius
+	//returns true if there were parts/neutral robots to go to; else returns false
+	public static boolean getToParts(RobotController rc) throws Exception {
+		if (rc.isCoreReady()) {
+			MapLocation goTo = null;
+			MapLocation myLoc = rc.getLocation();
+			int sightRadius = rc.getType().sensorRadiusSquared;
+			MapLocation[] squaresInSight = MapLocation.getAllMapLocationsWithinRadiusSq(rc.getLocation(), sightRadius);
+			RobotInfo[] nearbyNeutralRobots = rc.senseNearbyRobots(sightRadius, Team.NEUTRAL);
+
+			//goes to closest parts/neutral robot
+			if (nearbyNeutralRobots.length > 0) {
+				goTo = nearbyNeutralRobots[0].location;
+				for (RobotInfo n : nearbyNeutralRobots) {
+					if (myLoc.distanceSquaredTo(n.location) < myLoc.distanceSquaredTo(goTo)) {
+						goTo = n.location;
+					}
+				}
+			}
+			for (MapLocation sq : squaresInSight) {
+				if ((rc.senseParts(sq) > 0) && (goTo != null)) {
+					if (myLoc.distanceSquaredTo(sq) < myLoc.distanceSquaredTo(goTo)) {
+						goTo = sq;
+					}
+				}
+			}
+			if (goTo != null) {
+				MapLocation curLoc = rc.getLocation();
+				Direction dirToGo = curLoc.directionTo(goTo);
+				if (rc.canMove(dirToGo)) {
+					rc.move(dirToGo);
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+
+	//returns an int value for a direction
+	public static int dirToInt(Direction d) {
+		int curDir = 0;
+		switch (d) {
+		case NORTH:
+			curDir = 0;
+			break;
+		case NORTH_EAST:
+			curDir = 1;
+			break;
+		case EAST:
+			curDir = 2;
+			break;
+		case SOUTH_EAST:
+			curDir = 3;
+			break;
+		case SOUTH:
+			curDir = 4;
+			break;
+		case SOUTH_WEST:
+			curDir = 5;
+			break;
+		case WEST:
+			curDir = 6;
+			break;
+		case NORTH_WEST:
+			curDir = 7;
+			break;
+		}
+		return curDir;
+	}
+
+	//converts int to direction
+	public static Direction intToDir(int i) {
+		Direction curDir = Direction.NORTH;
+		switch (i) {
+		case 0:
+			curDir = Direction.NORTH;
+			break;
+		case 1:
+			curDir = Direction.NORTH_EAST;
+			break;
+		case 2:
+			curDir = Direction.EAST;
+			break;
+		case 3:
+			curDir = Direction.SOUTH_EAST;
+			break;
+		case 4:
+			curDir = Direction.SOUTH;
+			break;
+		case 5:
+			curDir = Direction.SOUTH_WEST;
+			break;
+		case 6:
+			curDir = Direction.WEST;
+			break;
+		case 7:
+			curDir = Direction.NORTH_WEST;
+			break;
+		}
+		return curDir;
+	}
+	
+	private static int mod8(int num) {
+		return ((num % 8) + 8) % 8;
+	}
+
+}
